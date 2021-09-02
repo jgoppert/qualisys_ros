e0b8b15e0eb6ef4243b7478c8b3aa1fae6f2a606
James Goppert <james.goppert@gmail.com>
2021-09-02 09:42:22 -0400

    Add image combine script.

--- scripts/image_combine.py
+++ scripts/image_combine.py
@@ -2,23 +2,23 @@
 import rospy
 import message_filters
 from sensor_msgs.msg import Image, CameraInfo
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 import cv2
-
-pub_img = rospy.Publisher('vr_image', Image, queue_size=1)
-bridge = CvBridge()
 
 
 def callback(image_left, image_right):
-    rospy.loginfo('callback')
     left = bridge.imgmsg_to_cv2(image_left, "bgr8")
     right = bridge.imgmsg_to_cv2(image_right, "bgr8")
     img_concat = cv2.hconcat([left, right])
-    pub_img.publish(bridge.cv2_to_imgmsg(img_concat, "bgr8"))
-
+    try:
+        pub_img.publish(bridge.cv2_to_imgmsg(img_concat, "bgr8"))
+    except CvBridgeError as e:
+        print(e)
 
 rospy.init_node('image_combine')
-rospy.loginfo('starting')
+pub_img = rospy.Publisher('vr_image', Image, queue_size=1)
+bridge = CvBridge()
+rospy.loginfo('image combine starting')
 image_left = message_filters.Subscriber('/virtual_cam/usb_cam/image_raw', Image)
 image_right = message_filters.Subscriber('/virtual_cam/usb_cam/image_raw', Image)
 ts = message_filters.TimeSynchronizer([image_left, image_right], 1)
